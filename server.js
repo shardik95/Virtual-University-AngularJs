232e5f3ca968eadf4555472a81594c15e34f3ee3
shardik95 <shardik95@ccs.neu.edu>
2018-06-19 00:46:44 -0400

    server added

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
 app.use(express.static('./dist/your_app_name'));
 
 app.get('/*', function(req,res) {
@@ -12,5 +11,4 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname,'/dist/angular-course-manager/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
